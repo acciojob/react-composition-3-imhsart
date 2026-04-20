8cb09f4ae7d74c6fa971acb744b4a74fc04568f8
imhsart <nitinrana2519@gmail.com>
2026-04-20 16:46:24 +0530

    Assignment attempt 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tooltip from "./Tooltip";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+        <Tooltip text='This is a tooltip'>
+          <h1>Hover over me</h1>
+        </Tooltip>
+        <hr/>
+        
+         <Tooltip text='This is another tooltip'>
+          <p>Hover over me to see another tooltip</p>
+        </Tooltip>
+        <hr/>
     </div>
   )
 }
